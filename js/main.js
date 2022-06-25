fdb14c9d25478171abb6f84c7c342b7c5dde54bb
Omar Mahdy <72965518+mahdtomar@users.noreply.github.com>
2022-06-25 20:04:04 +0200

    .

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const leftArrow =  document.querySelector(".leftarrow")
+const rightArrow =  document.querySelector(".rightarrow")
+let themeArray = ["images\desktop-image-hero-1.jpg","images\desktop-image-hero-2.jpg","images\desktop-image-hero-3.jpg"]
+let current = 0;
+
+leftArrow.addEventListener("click",(e)=>{
+    if(current=0){
+    
+    }
+})
+
+console.log(current)
