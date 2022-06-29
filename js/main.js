7670d27dc2a3f6a6fb95663d44acba063f2221ec
Omar Mahdy <72965518+mahdtomar@users.noreply.github.com>
2022-06-29 16:38:42 +0200

    changing the text is working now

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 let navigationArrows = document.querySelectorAll(".arrow");
 const mainImage = document.querySelector(".big");
-
+const one = document.querySelector(".content-one")
+const two = document.querySelector(".content-two")
+const three = document.querySelector(".content-three")
+const paragraphContainer = document.querySelector(".content")
+console.log(paragraphContainer)
 navigationArrows[0].addEventListener("click", () => {
   navigations("left");
 });
@@ -24,18 +28,24 @@ function left() {
       Url[27] = "3";
       newImage = Url.join("");
       mainImage.style.backgroundImage = `${newImage}`;
+      paragraphContainer.innerHTML = '';
+      paragraphContainer.appendChild(three)
       break;
 
     case "2":
       Url[27] = "1";
       newImage = Url.join("");
       mainImage.style.backgroundImage = `${newImage}`;
+      paragraphContainer.innerHTML = '';
+      paragraphContainer.appendChild(one)
       break;
 
     case "3":
       Url[27] = "2";
       newImage = Url.join("");
       mainImage.style.backgroundImage = `${newImage}`;
+      paragraphContainer.innerHTML = '';
+      paragraphContainer.appendChild(two)
       break;
 
     default:
@@ -49,18 +59,24 @@ function right() {
       Url[27] = "2";
       newImage = Url.join("");
       mainImage.style.backgroundImage = `${newImage}`;
+      paragraphContainer.innerHTML = '';
+      paragraphContainer.appendChild(two)
       break;
 
     case "2":
       Url[27] = "3";
       newImage = Url.join("");
       mainImage.style.backgroundImage = `${newImage}`;
+      paragraphContainer.innerHTML = '';
+      paragraphContainer.appendChild(three)
       break;
 
     case "3":
       Url[27] = "1";
       newImage = Url.join("");
       mainImage.style.backgroundImage = `${newImage}`;
+      paragraphContainer.innerHTML = '';
+      paragraphContainer.appendChild(one)
       break;
 
     default:
