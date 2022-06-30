adcc0a4dda88f23eca9b21dafbe3bea35a44df8e
Omar Mahdy <72965518+mahdtomar@users.noreply.github.com>
2022-06-30 09:42:25 +0200

    .

--- js/main.js
+++ js/main.js
@@ -1,24 +1,28 @@
 let navigationArrows = document.querySelectorAll(".arrow");
 const mainImage = document.querySelector(".big");
-const one = document.querySelector(".content-one")
-const two = document.querySelector(".content-two")
-const three = document.querySelector(".content-three")
-const paragraphContainer = document.querySelector(".content")
+const one = document.querySelector(".content-one");
+const two = document.querySelector(".content-two");
+const three = document.querySelector(".content-three");
+const paragraphContainer = document.querySelector(".content");
 navigationArrows[0].addEventListener("click", () => {
   navigations("left");
 });
 navigationArrows[1].addEventListener("click", () => {
   navigations("right");
 });
-console.log("global")
+console.log("global");
 function navigations(e) {
   let array = getComputedStyle(mainImage);
   let numberPosition = array.backgroundImage.split("");
   globalThis.Url = numberPosition.splice(27, numberPosition.length);
   let neededText = `url("../`;
   Url.unshift(neededText);
-  e == "left" ? left() : right();
-  console.log("eventlistener")
+  if (e == "left") {
+    left();
+  } else {
+    right();
+  }
+  console.log("eventlistener");
 }
 function left() {
   let newImage = "";
@@ -27,24 +31,24 @@ function left() {
       Url[27] = "3";
       newImage = Url.join("");
       mainImage.style.backgroundImage = `${newImage}`;
-      paragraphContainer.innerHTML = '';
-      paragraphContainer.appendChild(three)
+      paragraphContainer.innerHTML = "";
+      paragraphContainer.appendChild(three);
       break;
 
     case "2":
       Url[27] = "1";
       newImage = Url.join("");
       mainImage.style.backgroundImage = `${newImage}`;
-      paragraphContainer.innerHTML = '';
-      paragraphContainer.appendChild(one)
+      paragraphContainer.innerHTML = "";
+      paragraphContainer.appendChild(one);
       break;
 
     case "3":
       Url[27] = "2";
       newImage = Url.join("");
       mainImage.style.backgroundImage = `${newImage}`;
-      paragraphContainer.innerHTML = '';
-      paragraphContainer.appendChild(two)
+      paragraphContainer.innerHTML = "";
+      paragraphContainer.appendChild(two);
       break;
 
     default:
@@ -58,24 +62,24 @@ function right() {
       Url[27] = "2";
       newImage = Url.join("");
       mainImage.style.backgroundImage = `${newImage}`;
-      paragraphContainer.innerHTML = '';
-      paragraphContainer.appendChild(two)
+      paragraphContainer.innerHTML = "";
+      paragraphContainer.appendChild(two);
       break;
 
     case "2":
       Url[27] = "3";
       newImage = Url.join("");
       mainImage.style.backgroundImage = `${newImage}`;
-      paragraphContainer.innerHTML = '';
-      paragraphContainer.appendChild(three)
+      paragraphContainer.innerHTML = "";
+      paragraphContainer.appendChild(three);
       break;
 
     case "3":
       Url[27] = "1";
       newImage = Url.join("");
       mainImage.style.backgroundImage = `${newImage}`;
-      paragraphContainer.innerHTML = '';
-      paragraphContainer.appendChild(one)
+      paragraphContainer.innerHTML = "";
+      paragraphContainer.appendChild(one);
       break;
 
     default:
