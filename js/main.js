5469e19877e96d12e0cae41b7cebdf90fb62ba98
Omar Mahdy <72965518+mahdtomar@users.noreply.github.com>
2022-07-01 11:35:31 +0200

    whole new navigation function

--- js/main.js
+++ js/main.js
@@ -4,57 +4,89 @@ const one = document.querySelector(".content-one");
 const two = document.querySelector(".content-two");
 const three = document.querySelector(".content-three");
 const paragraphContainer = document.querySelector(".content");
-console.log(navigationArrows, mainImage, one, two, three, paragraphContainer);
+// console.log(navigationArrows, mainImage, one, two, three, paragraphContainer);
+const imageRelativPath = `../images/desktop-image-hero-2.jpg`;
+let someShit = `idk this is some shit${imageRelativPath}`;
+// console.log(someShit);
+// console.log(someShit.split("").splice(-34));
+
+// console.log(imageRelativPath.split("").length);
 navigationArrows[0].addEventListener("click", () => {
   navigations("left");
 });
 navigationArrows[1].addEventListener("click", () => {
   navigations("right");
 });
-console.log("this is global !",'cases should work');
+// console.log("this is global !", "cases should work");
 function navigations(e) {
   let array = getComputedStyle(mainImage);
-  let numberPosition = array.backgroundImage.split("");
-  console.log(numberPosition)
-  globalThis.imgPath = numberPosition.splice(27, numberPosition.length);
-  console.log(imgPath)
-  let neededText = `url("../`;
-  imgPath.unshift(neededText);
+  globalThis.numberPosition = array.backgroundImage.split("/");
+  globalThis.numberHorder =
+    numberPosition[numberPosition.length - 1].split("-");
+  globalThis.finalWord = numberHorder[numberHorder.length - 1];
+  globalThis.finalWordArray = finalWord.split("");
+
   if (e == "left") {
     left();
-    console.log("left is working");
   } else {
-    console.log("right is working");
     right();
   }
+  //-6
 }
 function left() {
   console.log("left");
-  let newImage = "";
-  switch (imgPath[27]) {
+  let newNumberHorder;
+  switch (finalWordArray[0]) {
     case "1":
-      console.log("case 1")
-      imgPath[27] = "3";
-      newImage = imgPath.join("");
-      mainImage.style.backgroundImage = `${newImage}`;
+      finalWordArray[0] = "3";
+      finalWord = finalWordArray.join("");
+      console.log(finalWord, finalWordArray);
+      numberHorder.pop();
+      numberHorder.push(finalWord);
+      console.log(numberHorder);
+      newNumberHorder = numberHorder.join("-");
+      numberPosition.pop();
+      numberPosition.push(newNumberHorder);
+      console.log(numberPosition);
+      imgPath = numberPosition.join("/");
+      console.log(imgPath);
+      console.log("case 1");
+      mainImage.style.backgroundImage = `${imgPath}`;
       paragraphContainer.innerHTML = "";
       paragraphContainer.appendChild(three);
       break;
-      
-      case "2":
-      console.log("case 2")
-      imgPath[27] = "1";
-      newImage = imgPath.join("");
-      mainImage.style.backgroundImage = `${newImage}`;
+
+    case "2":
+      finalWordArray[0] = "1";
+      finalWord = finalWordArray.join("");
+      console.log(finalWord, finalWordArray);
+      numberHorder.pop();
+      numberHorder.push(finalWord);
+      console.log(numberHorder);
+      newNumberHorder = numberHorder.join("-");
+      numberPosition.pop();
+      numberPosition.push(newNumberHorder);
+      console.log(numberPosition);
+      imgPath = numberPosition.join("/");
+      console.log(imgPath);
+      console.log("case 1");
+      mainImage.style.backgroundImage = `${imgPath}`;
       paragraphContainer.innerHTML = "";
       paragraphContainer.appendChild(one);
       break;
-      
-      case "3":
-      console.log("case 3")
-      imgPath[27] = "2";
-      newImage = imgPath.join("");
-      mainImage.style.backgroundImage = `${newImage}`;
+
+    case "3":
+      finalWordArray[0] = "2";
+      finalWord = finalWordArray.join("");
+      numberHorder.pop();
+      numberHorder.push(finalWord);
+      newNumberHorder = numberHorder.join("-");
+      numberPosition.pop();
+      numberPosition.push(newNumberHorder);
+      imgPath = numberPosition.join("/");
+      console.log(imgPath);
+      console.log("case 1");
+      mainImage.style.backgroundImage = `${imgPath}`;
       paragraphContainer.innerHTML = "";
       paragraphContainer.appendChild(two);
       break;
@@ -65,31 +97,52 @@ function left() {
 }
 function right() {
   console.log("right");
-  let newImage = "";
-  switch (imgPath[27]) {
+  let newNumberHorder;
+  switch (finalWordArray[0]) {
     case "1":
-      console.log("case 1")
-      imgPath[27] = "2";
-      newImage = imgPath.join("");
-      mainImage.style.backgroundImage = `${newImage}`;
+      finalWordArray[0] = "2";
+      finalWord = finalWordArray.join("");
+      numberHorder.pop();
+      numberHorder.push(finalWord);
+      newNumberHorder = numberHorder.join("-");
+      numberPosition.pop();
+      numberPosition.push(newNumberHorder);
+      imgPath = numberPosition.join("/");
+      console.log(imgPath);
+      console.log("case 1");
+      mainImage.style.backgroundImage = `${imgPath}`;
       paragraphContainer.innerHTML = "";
       paragraphContainer.appendChild(two);
       break;
-      
-      case "2":
-        console.log("case 2")
-        imgPath[27] = "3";
-        newImage = imgPath.join("");
-        mainImage.style.backgroundImage = `${newImage}`;
-        paragraphContainer.innerHTML = "";
-        paragraphContainer.appendChild(three);
-        break;
-        
-        case "3":
-          console.log("case 3")
-          imgPath[27] = "1";
-      newImage = imgPath.join("");
-      mainImage.style.backgroundImage = `${newImage}`;
+
+    case "2":
+      finalWordArray[0] = "3";
+      finalWord = finalWordArray.join("");
+      numberHorder.pop();
+      numberHorder.push(finalWord);
+      newNumberHorder = numberHorder.join("-");
+      numberPosition.pop();
+      numberPosition.push(newNumberHorder);
+      imgPath = numberPosition.join("/");
+      console.log(imgPath);
+      console.log("case 2");
+      mainImage.style.backgroundImage = `${imgPath}`;
+      paragraphContainer.innerHTML = "";
+      paragraphContainer.appendChild(three);
+      break;
+
+    case "3":
+      finalWordArray[0] = "1";
+      finalWord = finalWordArray.join("");
+      numberHorder.pop();
+      numberHorder.push(finalWord);
+      newNumberHorder = numberHorder.join("-");
+      numberPosition.pop();
+      numberPosition.push(newNumberHorder);
+      imgPath = numberPosition.join("/");
+      console.log(imgPath);
+      console.log("case 3");
+      mainImage.style.backgroundImage = `${imgPath}`;
       paragraphContainer.innerHTML = "";
       paragraphContainer.appendChild(one);
       break;
